dfe22dec3e4a88591f55e917681b3b2b4e4004c9
KasiaH <kah.gda@wp.pl>
2024-06-05 13:09:16 +0200

    created message application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, title: 'Wiadomość numer 1', body: 'Zawartość wiadomości nr 1...' },
+  { id: 2, title: 'Wiadomość numer 2', body: 'Zawartość wiadomości nr 2...' },
+]
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość wiadomości nr ${index}...`
+  })
+},3000)
+
+class App extends Component {
+  state = {
+    comments: [...data]
+  }
+
+  getData = () => {
+    if (this.state.comments.length !== data.length) {
+      console.log("aktualizacja");
+      this.setState({
+        comments: [...data]
+      })
+    } else {
+      console.log ("dane takie same - nie aktualizuje");
+    }
+  }
+  
+  componentDidMount() {
+    this.idI = setInterval(this.getData,1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idI)
+  }
+
+  render() {
+    const comments = this.state.comments.map(comment => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ))
+    //console.log(comments);
+    return (
+      <div className='App'>
+        {comments.reverse()}
+      </div>
+    )
+  }
 }
 
 export default App;
